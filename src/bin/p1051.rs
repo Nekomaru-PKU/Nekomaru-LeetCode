9437101206d425085df2dd15bba026c2051a6fb4
Nekomaru-PKU <Nekomaru@pku.edu.cn>
2024-06-10 15:00:21 +0800

    Daily Challenge 1051. Height Checker

---
+++ src/bin/p1051.rs
@@ -0,0 +1,13 @@
+mod solution {
+    pub fn main(vec: Vec<i32>) -> i32 {
+        let mut vec_sorted = vec.clone();
+        vec_sorted.sort_unstable();
+        vec.iter().zip(vec_sorted).map(|(&i, j)| if i != j {1} else {0}).sum()
+    }
+}
+
+fn main() {
+    assert_eq!(solution::main(vec![1,1,4,2,1,3]), 3);
+    assert_eq!(solution::main(vec![5,1,2,3,4]), 5);
+    assert_eq!(solution::main(vec![1,2,3,4,5]), 0);
+}
