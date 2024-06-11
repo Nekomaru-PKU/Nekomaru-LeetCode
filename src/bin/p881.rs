3bf4f0ea30d0eb09a7b49fbbc08ecc18a42bdf46
Nekomaru-PKU <Nekomaru@pku.edu.cn>
2024-06-11 15:46:59 +0800

    Optimize 881. Boats to Save People

--- src/bin/p881.rs
+++ src/bin/p881.rs
@@ -1,69 +1,20 @@
 mod solution {
-    use super::*;
-
     pub fn main(mut people: Vec<i32>, limit: i32) -> i32 {
         people.sort_unstable();
-
-        let mut taken = Vec::new();
-        taken.resize(people.len(), false);
-
+        let mut begin = 0;
+        let mut end = people.len() - 1; // note that `end` is inclusive!!
         let mut boats = 0;
-        for i in (0..people.len()).rev() {
-            if taken[i] {
-                continue;
+        while begin < end {
+            if people[begin] + people[end] <= limit {
+                begin += 1;
             }
-
-            // here `people[i]` is the heaviest person that we haven't put into
-            // a boat yet.
-            // let's find the haviest `people[j]` that can fit into a boat with
-            // `people[i]`.
-            let j = binary_search::find_last_element_less_equal_than(
-                &people[0..i],
-                limit - people[i]);
-            if let Some(j) = j {
-                for j_not_taken in (0..=j).rev() {
-                    if !taken[j_not_taken] {
-                        taken[j_not_taken] = true;
-                        break;
-                    }
-                }
-            }
+            end -= 1;
             boats += 1;
         }
-
+        if begin == end { // we have one person left.
+            boats += 1;
+        }
         boats
-    }
-
-}
-
-mod binary_search {
-    pub fn find_last_element_less_equal_than(nums: &[i32], target: i32) -> Option<usize> {
-        if nums.is_empty() || nums[0] > target {
-            return None;
-        }
-
-        let mut begin = 0;
-        let mut end = nums.len();
-        while end - begin > 1 {
-            let mid = begin + (end - begin) / 2;
-            if nums[mid] > target {
-                end = mid;
-            } else {
-                begin = mid;
-            }
-        }
-        Some(begin)
-    }
-
-    #[test]
-    fn test_find_last_element_less_equal_than() {
-        assert_eq!(find_last_element_less_equal_than(&[1, 2, 3, 4, 5], 0), None);
-        assert_eq!(find_last_element_less_equal_than(&[1, 2, 3, 4, 5], 1), Some(0));
-        assert_eq!(find_last_element_less_equal_than(&[1, 2, 3, 4, 5], 2), Some(1));
-        assert_eq!(find_last_element_less_equal_than(&[1, 2, 3, 4, 5], 3), Some(2));
-        assert_eq!(find_last_element_less_equal_than(&[1, 2, 3, 4, 5], 4), Some(3));
-        assert_eq!(find_last_element_less_equal_than(&[1, 2, 3, 4, 5], 5), Some(4));
-        assert_eq!(find_last_element_less_equal_than(&[1, 2, 3, 4, 5], 6), Some(4));
     }
 }
 
@@ -74,9 +25,11 @@ fn main() {
     assert_eq!(solution::main(vec![3, 2, 2, 1], 3), 3);
     assert_eq!(solution::main(vec![3, 5, 3, 4], 5), 4);
 
-    assert_eq!(solution::main(vec![1, 2, 3, 4], 5), 2);
-
     assert_eq!(solution::main(vec![5, 1, 7, 4, 2, 4], 7), 4);
+
+    assert_eq!(solution::main(vec![5, 5, 5, 5, 5], 5), 5);
+    assert_eq!(solution::main(vec![1, 2, 3, 4, 5], 6), 3);
+    assert_eq!(solution::main(vec![1, 2, 3, 4], 5), 2);
 
     let people = (1..=50000).map(|_| 50000).collect::<Vec<_>>();
     let result = print_time("perf", || solution::main(people, 50000));
