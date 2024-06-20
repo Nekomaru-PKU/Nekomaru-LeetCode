0a4ff8a24da7da7542afb4c10fde4cf6bb97a267
Nekomaru-PKU <Nekomaru@pku.edu.cn>
2024-06-21 00:10:35 +0800

    Daily Challenge 1552. Magnetic Force Between Two Balls

---
+++ src/bin/p1552.rs
@@ -0,0 +1,44 @@
+mod solution {
+    pub fn main(mut pos: Vec<i32>, m: i32) -> i32 {
+        if (pos.len() as i32) < m {
+            return 0;
+        }
+
+        pos.sort_unstable();
+        let pos_min = pos[0];
+        let pos_max = pos[pos.len() - 1];
+
+        let mut max = (pos_max - pos_min) / (m - 1);
+        let mut min = 1;
+        while min != max {
+            let mid = min + (max - min + 1) / 2;
+            if check(&pos, m, mid) {
+                min = mid;
+            } else {
+                max = mid - 1;
+            }
+        }
+        min
+    }
+
+    fn check(pos: &[i32], m: i32, dist: i32) -> bool {
+        // we always put a ball into the first basket.
+        let mut ball = 1;
+        let mut prev = pos[0];
+        for &pos in &pos[1..] {
+            if pos - prev >= dist {
+                prev = pos;
+                ball += 1;
+                if ball >= m {
+                    return true;
+                }
+            }
+        }
+        false
+    }
+}
+
+fn main() {
+    assert_eq!(solution::main(vec![1, 2, 3, 4, 7], 3), 3);
+    assert_eq!(solution::main(vec![5, 4, 3, 2, 1, 1000000000], 2), 999999999);
+}
