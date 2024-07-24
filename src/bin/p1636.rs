aa9eeb89ce1431226219f98e2e4af81bf1018648
Nekomaru-PKU <Nekomaru@pku.edu.cn>
2024-07-24 18:33:13 -0400

    Update p1636 and p2418 for more idiomatic iterator usages

--- src/bin/p1636.rs
+++ src/bin/p1636.rs
@@ -1,18 +1,17 @@
 mod solution {
-    pub fn main(nums: Vec<i32>) -> Vec<i32> {
-        use std::iter;
+    use std::iter;
 
+    pub fn main(nums: Vec<i32>) -> Vec<i32> {
         let mut freq = [0; 201];
         for num in nums {
             freq[num as usize + 100] += 1;
         }
-
         let mut freq = (-100..=100)
             .zip(freq)
             .collect::<Vec<_>>();
-        freq.sort_by_key(|&(num, freq)| (freq, -num));
-        freq.into_iter()
-            .flat_map(|(num, freq)| iter::repeat(num).take(freq))
+        freq.sort_unstable_by_key(|&(num, freq)| (freq, -num));
+        freq.iter()
+            .flat_map(|&(num, freq)| iter::repeat(num).take(freq))
             .collect()
     }
 }
@@ -21,4 +20,10 @@ fn main() {
     assert_eq!(solution::main(
         vec![1, 1, 2, 2, 2, 3]),
         vec![3, 1, 1, 2, 2, 2]);
+    assert_eq!(solution::main(
+        vec![2, 3, 1, 3, 2]),
+        vec![1, 3, 3, 2, 2]);
+    assert_eq!(solution::main(
+        vec![-1,  1, -6, 4,  5, -6, 1, 4, 1]),
+        vec![ 5, -1,  4, 4, -6, -6, 1, 1, 1]);
 }
--- src/bin/p2418.rs
+++ src/bin/p2418.rs
@@ -1,11 +1,26 @@
 mod solution {
     pub fn main(names: Vec<String>, heights: Vec<i32>) -> Vec<String> {
-        let mut zipped = names.into_iter().zip(heights).collect::<Vec<_>>();
-        zipped.sort_by_key(|&(_, height)| -height);
-        zipped.into_iter().map(|(name, _)| name).collect()
+        let mut zipped = names.into_iter()
+            .zip(heights)
+            .collect::<Vec<_>>();
+        zipped.sort_unstable_by_key(|&(_, height)| -height);
+        zipped.into_iter()
+            .map(|(name, _)| name)
+            .collect()
     }
 }
 
 fn main() {
-    let _ = solution::main(Vec::new(), Vec::new());
+    fn vec_of_string(vec: &[&'static str]) -> Vec<String> {
+        vec.iter().cloned().map(Into::into).collect()
+    }
+
+    assert_eq!(solution::main(
+        vec_of_string(&["Mary","John","Emma"]),
+        vec![180, 165, 170]),
+        vec_of_string(&["Mary","Emma","John"]));
+    assert_eq!(solution::main(
+        vec_of_string(&["Alice","Bob","Bob"]),
+        vec![155,185,150]),
+        vec_of_string(&["Bob","Alice","Bob"]));
 }
