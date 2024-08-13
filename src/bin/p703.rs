3b73b7c1ccff3006e9c4e6b7cc13c2721b3fbaff
Nekomaru-PKU <Nekomaru@pku.edu.cn>
2024-08-13 14:25:44 -0400

    Optimize 703. Kth Largest Element in a Stream

--- src/bin/p703.rs
+++ src/bin/p703.rs
@@ -1,26 +1,40 @@
 use std::collections::BinaryHeap;
 use std::cmp::Reverse;
 
-struct KthLargest(BinaryHeap<Reverse<i32>>);
+struct KthLargest{
+    heap: BinaryHeap<Reverse<i32>>,
+}
 
 impl KthLargest {
-    fn new(k: i32, mut nums: Vec<i32>) -> Self {
-        while nums.len() < k as _ {
-            nums.push(i32::MIN);
+    fn new(k: i32, nums: Vec<i32>) -> Self {
+        let mut heap = BinaryHeap::from(
+            vec![Reverse(i32::MIN); k as usize]);
+        for num in nums {
+            if num > heap.peek().unwrap().0 {
+                // here actually re-heapify twice is not
+                // necessary, but the standard library
+                // does not provide a method to replace
+                // top element of the heap and re-heapify.
+                heap.pop();
+                heap.push(Reverse(num));
+            }
         }
-        nums.sort_unstable();
-        Self(nums
-            .iter()
-            .rev()
-            .take(k as _)
-            .map(|&num| Reverse(num))
-            .collect())
+        Self { heap }
     }
 
     fn add(&mut self, num: i32) -> i32 {
-        self.0.push(Reverse(num));
-        self.0.pop();
-        self.0.peek().cloned().unwrap().0
+        if let Some(&Reverse(min)) = self.heap.peek() {
+            if num > min {
+                self.heap.pop();
+                self.heap.push(Reverse(num));
+                self.heap.peek().unwrap().0
+            } else {
+                min
+            }
+        } else {
+            self.heap.push(Reverse(num));
+            num
+        }
     }
 }
 
