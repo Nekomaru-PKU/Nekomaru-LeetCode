3e2742489bc606daaaca4e9bd1a07a2afbb96f9a
Nekomaru-PKU <Nekomaru@pku.edu.cn>
2024-08-25 13:50:08 -0400

    11. Container With Most Water

---
+++ src/bin/p11.rs
@@ -0,0 +1,25 @@
+pub fn solution(height: Vec<i32>) -> i32 {
+    use std::cmp::Ordering;
+    let mut head = 0;
+    let mut tail = height.len() - 1;
+    let mut max_area = 0;
+    while head < tail {
+        max_area = max_area.max(
+            i32::min(height[head], height[tail]) *
+            (tail - head) as i32);
+        match i32::cmp(&height[head], &height[tail]) {
+            Ordering::Less    => head += 1,
+            Ordering::Greater => tail -= 1,
+            _ => {
+                head += 1;
+                tail -= 1;
+            }
+        }
+    }
+    max_area
+}
+
+fn main() {
+    assert_eq!(solution(vec![1, 8, 6, 2, 5, 4, 8, 3, 7]), 49);
+    assert_eq!(solution(vec![1, 1]), 1);
+}
