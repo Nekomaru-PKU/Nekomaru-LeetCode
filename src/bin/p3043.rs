2dc3f9d9fdd2433513fb18700ffb728999938d16
Nekomaru-PKU <Nekomaru@pku.edu.cn>
2024-09-24 21:55:29 -0400

    Refactor 3043. Find the Length of the Longest Common Prefix

--- src/bin/p3043.rs
+++ src/bin/p3043.rs
@@ -1,28 +1,56 @@
-mod solution {
+mod trie {
     const NUM_DICTS: usize = 2;
-    const MAX_DIGITS_I32: usize = 10;
 
     #[derive(Debug, Default)]
-    struct TrieNode {
+    pub struct TrieNode {
         children: [Option<Box<TrieNode>>; 10],
         is_prefix: [bool; NUM_DICTS],
     }
 
-    pub fn main(dict0: Vec<i32>, dict1: Vec<i32>) -> i32 {
-        let mut root = None;
-        for (dict_id, dict) in [(0, &dict0), (1, &dict1)] {
-            for &num in dict {
-                let (digits_buf, digits_len) = get_digits(num);
-                trie_insert(
-                    &mut root,
-                    &digits_buf[..digits_len],
-                    dict_id);
-            }
-        }
-        trie_longest_common_prefix(&root)
+    pub fn insert<T: Copy + Into<usize>>(
+        node: &mut Option<Box<TrieNode>>,
+        s: &[T],
+        dict_id: usize) -> bool {
+        let node = node.get_or_insert(Box::default());
+        node.is_prefix[dict_id] = 
+            s.is_empty() ||
+            self::insert(
+                &mut node.children[s[0].into()],
+                &s[1..],
+                dict_id);
+        node.is_prefix[dict_id]
     }
 
-    fn get_digits(mut num: i32) -> ([u8; MAX_DIGITS_I32], usize) {
+    pub fn longest_common_prefix(root: &Option<Box<TrieNode>>) -> i32 {
+        self::longest_path_with(root, |node| {
+            node.is_prefix == [true; NUM_DICTS]
+        }) - 1
+    }
+
+    /// Finds the longest path starting from `node` and going downwards
+    /// that each node on this path satisfies `pred`.
+    /// 
+    /// Returns the number of nodes on this path.
+    fn longest_path_with(
+        node: &Option<Box<TrieNode>>,
+        pred: fn(&TrieNode) -> bool) -> i32 {
+        node.as_ref()
+            .filter(|&node| pred(node))
+            .map(|node| 1 + {
+                node.children
+                    .iter()
+                    .map(|node| self::longest_path_with(node, pred))
+                    .max()
+                    .unwrap()
+            })
+            .unwrap_or_default()
+    }
+}
+
+mod int {
+    const MAX_DIGITS_I32: usize = 10;
+
+    pub fn get_digits_i32(mut num: i32) -> ([u8; MAX_DIGITS_I32], usize) {
         debug_assert!(num >= 1);
         let mut buf = [0; MAX_DIGITS_I32];
         let mut len = 0;
@@ -34,48 +62,23 @@ mod solution {
         buf[..len].reverse();
         (buf, len)
     }
+}
 
-    fn trie_insert<T: Copy + Into<usize>>(
-        node: &mut Option<Box<TrieNode>>,
-        vals: &[T],
-        dict_id: usize) -> bool {
-        let node = node.get_or_insert(Box::default());
-        node.is_prefix[dict_id] = 
-            vals.is_empty() ||
-            trie_insert(
-                &mut node.children[vals[0].into()],
-                &vals[1..],
+fn solution(dict0: Vec<i32>, dict1: Vec<i32>) -> i32 {
+    let mut root = None;
+    for (dict_id, dict) in [(0, &dict0), (1, &dict1)] {
+        for &num in dict {
+            let (digits_buf, digits_len) = int::get_digits_i32(num);
+            trie::insert(
+                &mut root,
+                &digits_buf[..digits_len],
                 dict_id);
-        node.is_prefix[dict_id]
+        }
     }
-
-    fn trie_longest_common_prefix(root: &Option<Box<TrieNode>>) -> i32 {
-        trie_longest_path_with(root, |node| {
-            node.is_prefix == [true; NUM_DICTS]
-        }) - 1
-    }
-
-    /// Finds the longest path starting from `node` and going downwards
-    /// that each node on this path satisfies `pred`.
-    /// 
-    /// Returns the number of nodes on this path.
-    fn trie_longest_path_with(
-        node: &Option<Box<TrieNode>>,
-        pred: fn(&TrieNode) -> bool) -> i32 {
-        node.as_ref()
-            .filter(|&node| pred(node))
-            .map(|node| 1 + {
-                node.children
-                    .iter()
-                    .map(|node| trie_longest_path_with(node, pred))
-                    .max()
-                    .unwrap()
-            })
-            .unwrap_or_default()
-    }
+    trie::longest_common_prefix(&root)
 }
 
 fn main() {
-    assert_eq!(solution::main(vec![1, 10, 100], vec![1000]), 3);
-    assert_eq!(solution::main(vec![1, 2, 3], vec![4, 4, 4]), 0);
+    assert_eq!(solution(vec![1, 10, 100], vec![1000]), 3);
+    assert_eq!(solution(vec![1, 2, 3], vec![4, 4, 4]), 0);
 }
